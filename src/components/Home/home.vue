88ae42bf83769df263c891de792d43bd25ede4db
cuiyanminminmin <787270301@qq.com>
2019-12-09 10:22:59 +0800

    beizhu

--- src/components/Home/home.vue
+++ src/components/Home/home.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>首页</div>
+  <div>
+    <span>首页</span>
+    <div class="get_address_btn" @click="getAddress('fang')">
+      获取fang地址
+    </div>
+    <div class="get_address_btn" @click="getAddress('cui')">
+      获取cui地址
+    </div>
+    <div class="show_address" v-text="address"></div>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
+import {getAddressApi} from '../../api/address';
 export default {
   name: "home",
   data() {
-    return {};
+    return {
+      address: ""
+    };
+  },
+  methods: {
+    getAddress (name) {
+      getAddressApi({name}).then(result => {
+        this.address = result.address;
+      });
+    }
   },
   components: {}
 };
