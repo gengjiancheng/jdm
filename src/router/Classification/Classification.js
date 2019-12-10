337347638a722f0816cc0a3721c2e326f9380298
gengjiancheng <791691421@qq.com>
2019-12-10 10:18:50 +0800

    第二天

--- src/router/Classification/Classification.js
+++ src/router/Classification/Classification.js
@@ -1,6 +1,14 @@
 import Classification from '../../components/Classification/classification.vue';
+import hot from '../../components/Classification/hot.vue'
 export default [{
     path: '/index/Classification',
     name: 'Classification',
-    component: Classification
+    component: Classification,
+    children:[
+        {
+            path:'/index/Classification/hot',
+            name:'hot',
+            component:hot
+        }
+    ]
 }]
