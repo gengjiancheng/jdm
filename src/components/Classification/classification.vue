52616d234cd0791dcbff1028af7e947849d62ac3
shihongbo1314 <1614452639@qq.com>
2019-12-10 09:42:38 +0800

    提交

--- src/components/Classification/classification.vue
+++ src/components/Classification/classification.vue
@@ -1,26 +1,234 @@
 <template>
-  <div>
-    <van-icon  class="log" name="arrow-left" />
-    <van-search v-model="value" placeholder="请输入搜索关键词" shape="round" @search="onSearch">
+  <div class="box">
+    <van-icon class="log" name="arrow-left" />
     <van-icon class="login" name="ellipsis" />
-    </van-search>
+    <van-search class="input" placeholder="请输入搜索关键词" shape="round"></van-search>
+    <van-tabs title-active-color="red">
+      <van-tab v-for="item in ary" :key="item.id" :title="item.title">
+        {{item.title}}
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 <script>
 // @ is an alias to /src
-import Vue from 'vue';
-import { Search } from 'vant';
-
-Vue.use(Search);
 export default {
   name: "classification",
   data() {
-    return {};
+    return {
+      ary: [
+        {
+          title: "手机数码",
+          id: 1
+        },
+        {
+          title: "家用电器",
+          id: 2
+        },
+        {
+          title: "计生情趣",
+          id: 3
+        },
+        {
+          title: "美妆护肤",
+          id: 4
+        },
+        {
+          title: "个护清洁",
+          id: 5
+        },
+        {
+          title: "美妆护肤",
+          id: 6
+        },
+        {
+          title: "汽车生活",
+          id: 7
+        },
+        {
+          title: "京东超市",
+          id: 8
+        },
+        {
+          title: "男鞋",
+          id: 9
+        },
+        {
+          title: "男装",
+          id: 10
+        },
+        {
+          title: "女装",
+          id: 11
+        },
+        {
+          title: "女鞋",
+          id: 12
+        },
+        {
+          title: "母婴童装",
+          id: 13
+        },
+        {
+          title: "图书音像",
+          id: 14
+        },
+        {
+          title: "运动户外",
+          id: 15
+        },
+        {
+          title: "女鞋",
+          id: 16
+        },
+        {
+          title: "内衣配饰",
+          id: 17
+        },
+        {
+          title: "食品生鲜",
+          id: 18
+        },
+        {
+          title: "酒水饮料",
+          id: 19
+        },
+         {
+          title: "家具家装",
+          id: 20
+        },
+         {
+          title: "家居厨具",
+          id: 21
+        },
+         {
+          title: "箱包手袋",
+          id: 22
+        },
+         {
+          title: "钟表珠宝",
+          id: 23
+        },
+         {
+          title: "玩具乐器",
+          id: 24
+        },
+         {
+          title: "医药保健",
+          id: 25
+        },
+         {
+          title: "宠物生活",
+          id: 26
+        },
+         {
+          title: "礼品鲜花",
+          id: 27
+        },
+         {
+          title: "农资绿植",
+          id: 28
+        },
+         {
+          title: "生活旅行",
+          id: 29
+        },
+        {
+          title: "奢侈品",
+          id: 30
+        },
+        {
+          title: "京东国际",
+          id: 31
+        },
+        {
+          title: "艺术邮币",
+          id: 32
+        },
+        {
+          title: "二手商品",
+          id: 33
+        },
+        {
+          title: "特产馆",
+          id: 34
+        },
+        {
+          title: "京东金融",
+          id: 35
+        },
+        {
+          title: "国际名牌",
+          id: 36
+        },
+        {
+          title: "拍卖",
+          id: 37
+        },
+        {
+          title: "房产",
+          id: 38
+        },
+        {
+          title: "工业品",
+          id: 39
+        },
+      ]
+    };
+  },
+  methods:{
+
   },
   components: {}
 };
 </script>
 <style lang="less">
-    
-    
+.box {
+  box-sizing: border-box;
+}
+.log {
+  float: left;
+  margin: 4vw 0 0 4vw;
+}
+.input {
+  margin: 2vw auto;
+  width: 80vw;
+}
+.login {
+  float: right;
+  margin: 4vw 4vw 0 0;
+}
+.van-tabs__wrap {
+  height: 100% !important;
+}
+.van-tabs__nav {
+  flex-direction: column;
+  height: 60vw;
+}
+.van-tabs__line {
+  background-color: #fff !important;
+}
+.van-tab {
+  height: 20vw;
+  width: 20vw;
+  box-sizing: none !important;
+}
+.van-tabs__nav--line {
+  width: 20vw;
+}
+.van-tabs__content {
+  width: 70vw;
+}
+.van-tab__pane {
+  position: absolute;
+  top: 5vw;
+  right: 0;
+  width: 75vw !important;
+}
+span {
+  line-height: 11vw;
+}
+.title-active-color {
+  color: aqua;
+}
 </style>
---
+++ src/components/Classification/hot.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       <router-view> hahah hhjj</router-view>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+export default {
+    name: 'XXX',
+    data() {
+        return {
+        
+        }
+    },
+    components: {
+        
+    }
+}
+</script>
+<style lang="less">
+
+</style>
