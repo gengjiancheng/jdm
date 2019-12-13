eff2882bc4ab0a5c77d725919beb8e04cfce0700
gengjiancheng <791691421@qq.com>
2019-12-13 23:38:55 +0800

    m

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,12 +1,11 @@
-export function changeHotList(state,option){
+export function changeHotList(state, option) {
     // 对于hotList 我们到底是整个替换 还是往上添加 ?
     //option newType oldType
-    localStorage.setItem('hotList',JSON.stringify(option))
-    if(option.newType == option.oldType){
+    localStorage.setItem('hotList', JSON.stringify(option))
+    if (option.newType == option.oldType) {
         console.log("进来了吗")
         state.hotList.push(...option)
-    }else{
+    } else {
         state.hotList = option
     }
-     
 }
